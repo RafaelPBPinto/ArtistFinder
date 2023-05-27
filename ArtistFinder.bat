df740b32f4dd17a39ba8912aee3e826facc5c852
Rafael Pinto <85125753+RafaelPBPinto@users.noreply.github.com>
2023-05-27 12:42:31 +0100

    Update ArtistFinder.bat

--- ArtistFinder.bat
+++ ArtistFinder.bat
@@ -1,4 +1,4 @@
 cd /d %~dp0
-start cmd.exe /k "pip install -r requirements.txt"
-start cmd.exe /k "cd backend && python manage.py runserver"
-start cmd.exe /k "cd artist_finder && flutter run && 1"
+start /min cmd.exe /k "pip install -r requirements.txt"
+start /min cmd.exe /k "cd backend && python manage.py runserver"
+start cmd.exe /k "cd artist_finder && flutter run"
